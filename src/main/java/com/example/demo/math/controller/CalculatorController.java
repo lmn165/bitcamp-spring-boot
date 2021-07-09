23d3a13cd8cea2ddcdda17cbaca89e6d0a37bd73
lmn165 <lastalking@naver.com>
2021-07-09 13:02:22 +0900

    while loop

--- src/main/java/com/example/demo/math/controller/CalculatorController.java
+++ src/main/java/com/example/demo/math/controller/CalculatorController.java
@@ -11,14 +11,56 @@ public class CalculatorController {
         Scanner scanner = new Scanner(System.in);
         CalculatorDTO calculator = new CalculatorDTO();
         CalculatorService calculatorService = new CalculatorServiceImpl();
-        System.out.println("수열의 시작값: ");
-        calculator.setNum1(scanner.nextInt());
-        System.out.println("수열의 마지막값: ");
-        calculator.setNum2(scanner.nextInt()); // 재사용성
-        int[] arr = calculatorService.sequence(calculator);
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + "\t");
+        while (true){
+            System.out.println("\n[메뉴] 0.종료 1.계산기 2.수열 ");
+            switch (scanner.next()){
+                case "0":   return; // return은 exit 코드이다.
+                case "1":
+                    System.out.println("숫자 1 ?");
+                    calculator.setNum1(scanner.nextInt());
+                    System.out.println("연산자: ");
+                    calculator.setOpcode(scanner.next());
+                    System.out.println("숫자 2 ?");
+                    calculator.setNum2(scanner.nextInt()); // 재사용성
+                    int result = 0;
+                    // if(조건부: Conditon) {}, switch(Conditon){}
+                    switch (calculator.getOpcode()){
+                        case "+":
+                            result = calculatorService.add(calculator);
+                            break;
+                        case "-":
+                            result = calculatorService.subtract(calculator);
+                            break;
+                        case "*":
+                            result = calculatorService.multiple(calculator);
+                            break;
+                        case "/":
+                            result = calculatorService.divide(calculator);
+                            break;
+                        case "%":
+                            result = calculatorService.remain(calculator);
+                            break;
+                    }
+
+                    System.out.printf("%d %s %d = %d",
+                            calculator.getNum1(),
+                            calculator.getOpcode(),
+                            calculator.getNum2(),
+                            result);
+                    break;
+                case "2":
+                    System.out.println("수열의 시작값: ");
+                    calculator.setNum1(scanner.nextInt());
+                    System.out.println("수열의 마지막값: ");
+                    calculator.setNum2(scanner.nextInt()); // 재사용성
+                    int[] arr = calculatorService.sequence(calculator);
+                    for(int i=0; i<arr.length; i++){
+                        System.out.print(arr[i] + "\t");
+                    }
+                    break;
+            }
         }
+
 /*
        int[] arr = calculatorService.sequence2(null);
        for (int i=0; i<arr.length; i++){
@@ -27,37 +69,7 @@ public class CalculatorController {
 
  */
         /*
-        System.out.println("숫자 1 ?");
-        calculator.setNum1(scanner.nextInt());
-        System.out.println("연산자: ");
-        calculator.setOpcode(scanner.next());
-        System.out.println("숫자 2 ?");
-        calculator.setNum2(scanner.nextInt()); // 재사용성
-        int result = 0;
-        // if(조건부: Conditon) {}, switch(Conditon){}
-        switch (calculator.getOpcode()){
-            case "+":
-                result = calculatorService.add(calculator);
-                break;
-            case "-":
-                result = calculatorService.subtract(calculator);
-                break;
-            case "*":
-                result = calculatorService.multiple(calculator);
-                break;
-            case "/":
-                result = calculatorService.divide(calculator);
-                break;
-            case "%":
-                result = calculatorService.remain(calculator);
-                break;
-        }
 
-        System.out.printf("%d %s %d = %d",
-                calculator.getNum1(),
-                calculator.getOpcode(),
-                calculator.getNum2(),
-                result);
 
          */
 
